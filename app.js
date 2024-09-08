695d117ae1eefd600170ff8bf6b512d8a76ac35f
osonoi <osonoi@hotmail.com>
2024-09-08 13:24:51 +0900

    add hell

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use('/hello', helloRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
